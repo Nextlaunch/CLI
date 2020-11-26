51e0197645f889b0e749ea051b8833762265a694
fatalcenturion <tom.b.2k2@gmail.com>
2020-11-26 22:22:56 +0000

    major boosts in efficiency, also now has compatible imagery for 33 agencies

--- build.sh
+++ build.sh
@@ -4,13 +4,19 @@ rustup target add x86_64-unknown-linux-gnu
 rustup target add x86_64-pc-windows-gnu
 rustup target add x86_64-apple-darwin
 
-
+mkdir target/
+mkdir target/x86_64-unknown-linux-gnu/
+mkdir target/x86_64-pc-windows-gnu/
+mkdir target/x86_64-apple-darwin/
+mkdir target/x86_64-unknown-linux-gnu/release/
+mkdir target/x86_64-pc-windows-gnu/release/
+mkdir target/x86_64-apple-darwin/release/
 
 echo "Building NextLaunch for Linux"
 
 {
   cargo build --release --target x86_64-unknown-linux-gnu
-} &>target/x86_64-unknown-linux-gnu/release/build.log
+} &> target/x86_64-unknown-linux-gnu/release/build.log
 
 cat target/x86_64-unknown-linux-gnu/release/build.log
 
@@ -20,7 +26,7 @@ echo "Building NextLaunch for Windows"
   cargo build --release --target x86_64-pc-windows-gnu
 } &>target/x86_64-pc-windows-gnu/release/build.log
 
-cat target/x86_64-windows-gnu/release/build.log
+cat target/x86_64-pc-windows-gnu/release/build.log
 
 echo "Building NextLaunch for Mac OS"
 
@@ -29,6 +35,5 @@ echo "Building NextLaunch for Mac OS"
 } &>target/x86_64-apple-darwin/release/build.log
 
 cat target/x86_64-apple-darwin/release/build.log
-
 
 echo "Build script complete"
