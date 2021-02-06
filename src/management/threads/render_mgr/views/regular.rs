51a3cf44227d2e275869473ecf7d6fa1542cc7bf
fatalcenturion <tom.b.2k2@gmail.com>
2021-02-06 01:09:59 +0000

    working on frame rendering again

--- src/management/threads/render_mgr/views/regular.rs
+++ src/management/threads/render_mgr/views/regular.rs
@@ -1,12 +1,28 @@
 use crate::management::data::RenderFrame;
-use crate::management::rendering::structures::Frame;
+use crate::management::rendering::structures::{Frame, Line, Alignment, Cell};
 
 pub async fn process(i: RenderFrame) -> Frame {
 
 
 
     let mut f = Frame {
-        cells: vec!()
+        rows: vec![
+            vec![
+                Cell {
+                    width: 0,
+                    height: 0,
+                    lines: vec![
+                        Line {
+                            align: Alignment::Middle,
+                            length: 0,
+                            content: "".to_string(),
+                            title: None,
+                            value: None,
+                        }
+                    ],
+                }
+            ]
+        ],
     };
 
 
