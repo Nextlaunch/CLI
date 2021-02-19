eea9393fd632e73041f4eb491e65bc46a0781609
fatalcenturion <tom.b.2k2@gmail.com>
2021-02-19 08:50:56 +0000

    sixels print, i found the problem lmao

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,12 @@ pub mod management;
 pub mod constants;
 pub mod utilities;
 
+pub mod sixel;
+
 
 #[tokio::main]
 async fn main() {
-    println!("\x1b[?8452h\n\x1b[Pq\n#0;2;99;99;99#1;2;0;99;0\n#0?o!${FULL}Oo\$#1??!${PART}_-\n\n\n#1!14@\x1b\\");
+    print!("{}", sixel::CONTENT);
 
     exit(0);
 
