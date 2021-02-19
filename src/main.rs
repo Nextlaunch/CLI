f9a0730f30d274b188ef70c7016561b57818db0d
fatalcenturion <tom.b.2k2@gmail.com>
2021-02-19 00:24:17 +0000

    messing with sixels, test tomorrow on Windows to see if they work there, not sure whats wrong here

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 pub use constants::*;
+use std::process::exit;
 
 
 pub mod management;
@@ -8,6 +9,10 @@ pub mod utilities;
 
 #[tokio::main]
 async fn main() {
+    println!("\x1b[?8452h\n\x1b[Pq\n#0;2;99;99;99#1;2;0;99;0\n#0?o!${FULL}Oo\$#1??!${PART}_-\n\n\n#1!14@\x1b\\");
+
+    exit(0);
+
     let flags = management::flags::check_flags().await;
 
     management::flags::process_flags(&flags).await;
