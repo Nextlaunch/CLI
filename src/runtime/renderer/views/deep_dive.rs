67395b350dbe8e9aa4be94c91e2b7b521966e46f
fatalcenturion <tom.b.2k2@gmail.com>
2021-03-07 10:09:14 +0000

    begin adding information for SpaceX Falcon 9 Block 5 to "detailed view"

--- src/runtime/renderer/views/deep_dive.rs
+++ src/runtime/renderer/views/deep_dive.rs
@@ -1,41 +1,72 @@
 use tui::Terminal;
 use tui::backend::CrosstermBackend;
 use std::io::Stdout;
-use crate::runtime::data::launches::structures::Launch;
+use crate::runtime::data::launches::structures::{Launch, Rocket};
 use tui::layout::{Layout, Direction, Constraint, Alignment, Rect};
 use tui::widgets::{Clear, Block, Borders, Paragraph};
 use tui::text::Text;
 
+pub mod dict;
 
 pub fn run(mut out: Terminal<CrosstermBackend<Stdout>>, launch_present: bool, i: &Option<Launch>) {
     if launch_present {
-        out.draw(|mut f| {
-            let size = f.size();
-
-            let chunks = Layout::default()
-                .direction(Direction::Horizontal)
-                .constraints([Constraint::Percentage(50), Constraint::Percentage(50)].as_ref())
-                .split(size);
-
-            let paragraph = Paragraph::new(Text::raw(""))
-                .block(Block::default().title(" Detailed Overview ").borders(Borders::ALL))
-                .alignment(Alignment::Left);
-            f.render_widget(paragraph, chunks[0]);
-
-            let paragraph = Paragraph::new(Text::raw(""))
-                .block(Block::default().borders(Borders::ALL))
-                .alignment(Alignment::Left);
-            f.render_widget(paragraph, chunks[1]);
-
-            let error = Paragraph::new(Text::raw("\nThere is currently no launch available.\nCheck this screen again once one is available."))
-                .block(Block::default().title(" Error ").borders(Borders::ALL))
-                .alignment(Alignment::Center);
-
-            let area = centered_rect(60, 20, size);
-
-            f.render_widget(Clear, area); //this clears out the background
-            f.render_widget(error, area);
+        let ln = i.clone().unwrap();
+        let rocket = ln.rocket.unwrap_or(Rocket {
+            id: None,
+            configuration: None
         });
+        let vehicle_id = dict::match_rocket(rocket.id.unwrap_or(0));
+
+        if vehicle_id.is_some() {
+            out.draw(|mut f| {
+                let size = f.size();
+
+                let chunks = Layout::default()
+                    .direction(Direction::Horizontal)
+                    .constraints([Constraint::Percentage(50), Constraint::Percentage(50)].as_ref())
+                    .split(size);
+
+                let paragraph = Paragraph::new(Text::raw(""))
+                    .block(Block::default().title(" Detailed Overview ").borders(Borders::ALL))
+                    .alignment(Alignment::Left);
+                f.render_widget(paragraph, chunks[0]);
+
+                let paragraph = Paragraph::new(Text::raw(""))
+                    .block(Block::default().borders(Borders::ALL))
+                    .alignment(Alignment::Left);
+                f.render_widget(paragraph, chunks[1]);
+            });
+        } else {
+            out.draw(|mut f| {
+                let size = f.size();
+
+                let chunks = Layout::default()
+                    .direction(Direction::Horizontal)
+                    .constraints([Constraint::Percentage(50), Constraint::Percentage(50)].as_ref())
+                    .split(size);
+
+                let paragraph = Paragraph::new(Text::raw(""))
+                    .block(Block::default().borders(Borders::ALL))
+                    .alignment(Alignment::Left);
+                f.render_widget(paragraph, chunks[0]);
+
+                let paragraph = Paragraph::new(Text::raw(""))
+                    .block(Block::default().title("Detailed Overview").borders(Borders::ALL))
+                    .alignment(Alignment::Left);
+                f.render_widget(paragraph, chunks[1]);
+
+                let error = Paragraph::new(Text::raw("There is currently no detailed overview available for this rocket.\nIt may be coming in a future patch."))
+                    .block(Block::default().title("Popup").borders(Borders::ALL))
+                    .alignment(Alignment::Left);
+
+                let area = centered_rect(60, 20, size);
+
+                f.render_widget(Clear, area); //this clears out the background
+                f.render_widget(error, area);
+            });
+        }
+
+
     } else {
         out.draw(|mut f| {
             let size = f.size();
---
+++ src/runtime/renderer/views/deep_dive/dict/space_x/falcon_9/block_5.rs
@@ -0,0 +1,64 @@
+pub use super::super::super::{Rocket, Stage, Engine};
+use crate::runtime::renderer::views::deep_dive::dict::Fuel;
+
+pub fn wiki() -> Rocket {
+    Rocket {
+        name: "Falcon 9 Block 5".to_string(),
+        stages: vec![
+            Stage {
+                name: "Booster".to_string(),
+                engines: vec![
+                    Engine {
+                        count: 9,
+                        name: "Merlin 1D+".to_string(),
+                        class: "Atmospheric".to_string(),
+                        manufacturer: "SpaceX".to_string(),
+                        engine_type: "Bi-propellant".to_string(),
+                        thrust_sea: "845 kN".to_string(),
+                        thrust_vac: "981 kN".to_string(),
+                        thrust_weight_sea: "86,166 kgf".to_string(),
+                        thrust_weight_vac: "100,035 kgf".to_string(),
+                        cycle: "Gas Generator".to_string(),
+                        fuels: vec![
+                            Fuel {
+                                name: "Liquid Oxygen".to_string(),
+                                abbrev: "LOX".to_string(),
+                                class: "".to_string(),
+                                density_g_ml: "1.141 g/ml".to_string(),
+                                mass_kg_l: "1.141 kg/l".to_string()
+                            },
+                            Fuel {
+                                name: "Refined Petroleum 1".to_string(),
+                                abbrev: "RP-1".to_string(),
+                                class: "".to_string(),
+                                density_g_ml: "0.81".to_string(),
+                                mass_kg_l: "0.81 kg/l".to_string()
+                            }
+                        ],
+                        specific_impulse: "310 Seconds".to_string(),
+                        gimbal: "".to_string(),
+                        min_throttle: "57%".to_string(),
+                        min_throttle_as_newtons: "482 kN".to_string(),
+                        min_throttle_sea: "64%".to_string(),
+                        min_throttle_sea_as_newtons: "626 kN".to_string()
+                    }
+                ],
+                thrust_weight: "".to_string()
+            }
+        ],
+        reusable: "".to_string(),
+        entered_service: "".to_string(),
+        retired: false,
+        retired_date: "".to_string(),
+        human_rated: false,
+        capacity_leo: "".to_string(),
+        capacity_gto: "".to_string(),
+        capacity_mto: "".to_string(),
+        height: "".to_string(),
+        diameter: "".to_string(),
+        country: "".to_string(),
+        manufacturer: "".to_string(),
+        family: "".to_string(),
+        first_flight: "".to_string(),
+    }
+}
