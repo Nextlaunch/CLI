6c4c116baa8631a3054e2e0d88c25ab6706a423b
Altrius <tom.b.2k2@gmail.com>
2021-03-11 11:37:19 +0000

    implement build script which downloads and extracts required files (sorry rustaceans, it breaks the rules and uses system directories)

--- build.rs
+++ build.rs
@@ -6,6 +6,7 @@ use std::io::Write;
 
 const LANG_URL: &str = "https://raw.githubusercontent.com/AltriusRS/NextLaunch/next/languages/languages.zip";
 const CONFIG_URL: &str = "https://raw.githubusercontent.com/AltriusRS/NextLaunch/next/nextlaunch.json";
+const README_URL: &str = "https://raw.githubusercontent.com/AltriusRS/NextLaunch/next/documents/readme.zip";
 
 fn main() {
     let raw_data_dir = dirs_2::data_dir();
@@ -16,12 +17,47 @@ fn main() {
         let pack_response = reqwest::blocking::get(LANG_URL);
         println!("Downloading config files");
         let config_response = reqwest::blocking::get(CONFIG_URL);
+        println!("Downloading components");
+        let readme_response = reqwest::blocking::get(README_URL);
 
         if let Ok(raw_pack) = pack_response {
             println!("unpacking language files");
             let bytes = raw_pack.bytes();
             if let Ok(raw_bytes) = bytes {
                 let opts = fs::OpenOptions::new().read(true).write(true).create(true).open(format!("{}/lang_pack.tmpfile", dp));
+                if let Ok(mut file) = opts {
+                    let bytes: Vec<u8> = raw_bytes.to_vec();
+                    let _ = file.write_all(bytes.as_slice());
+                    let _ = file.flush();
+
+                    let mut archive = zip::ZipArchive::new(file).unwrap();
+
+                    if let Err(e) = archive.extract(format!("{}/languages", dp)) {
+                        println!("cargo:warning=NextLaunch failed to unpack language files");
+                        println!("cargo:warning={:#?}", e);
+                        exit(1);
+                    }
+                } else {
+                    println!("cargo:warning=NextLaunch failed to unpack language files");
+                    println!("cargo:warning={:#?}", opts.unwrap_err());
+                    exit(1);
+                }
+            } else {
+                println!("cargo:warning=NextLaunch failed to unpack language files");
+                println!("cargo:warning={:#?}", bytes.unwrap_err());
+                exit(1);
+            }
+        } else {
+            println!("cargo:warning=NextLaunch failed to download language files");
+            println!("cargo:warning={:#?}", pack_response.unwrap_err());
+            exit(1);
+        }
+
+        if let Ok(raw_pack) = readme_response {
+            println!("unpacking language files");
+            let bytes = raw_pack.bytes();
+            if let Ok(raw_bytes) = bytes {
+                let opts = fs::OpenOptions::new().read(true).write(true).create(true).open(format!("{}/readme_pack.tmpfile", dp));
                 if let Ok(mut file) = opts {
                     let bytes: Vec<u8> = raw_bytes.to_vec();
                     let _ = file.write_all(bytes.as_slice());
@@ -76,6 +112,7 @@ fn main() {
         }
 
         let _ = fs::remove_file(format!("{}/lang_pack.tmpfile", dp));
+        let _ = fs::remove_file(format!("{}/readme_pack.tmpfile", dp));
 
     } else {
         println!("cargo:warning=NextLaunch failed to determine location to store required files");
